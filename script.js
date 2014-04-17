e6a328060fd7e6a78ead742fed7685580ab64552
최희수 <heesu7563@naver.com>
2014-04-17 20:21:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,32 +2,32 @@ $(document).ready(function(){
 	    $('.controller').keydown(function(key) {
         switch(parseInt(key.which,10)) {
 				
-			$('.left-btn').click(function(){
+			$('#left-btn').click(function(){
 				$(this).animate({left: "-=10px"}, 'fast');
 				break;
 			});
 
-			$('.up-btn').click(function(){
+			$('#up-btn').click(function(){
 				$(this).animate({top: "-=10px"}, 'fast');
 			break;
 			});
 			
-			$('.right-btn').click(function(){
+			$('#right-btn').click(function(){
 				$('this').animate({left: "+=10px"}, 'fast');
 			break;
 			});
 			
-			$('.down-btn').click(function(){
-			$('img').animate({top: "+=10px"}, 'fast');
+			$('#down-btn').click(function(){
+				$('this').animate({top: "+=10px"}, 'fast');
 			break;
 			});
 		}
 	});
 	
-	 $('.in-btn').click(function() {
+	 $('#in-btn').click(function() {
          	$(this).fadeIn('fast');
     	 });
-    	 $('.out-btn').click(function() {
+    	 $('#out-btn').click(function() {
          	$(this).fadeOut('fast');
     	 });
 });
