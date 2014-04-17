5c5230b4b81ddaf67abc53dae7cd20cb33feef46
최희수 <heesu7563@naver.com>
2014-04-17 20:11:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -24,4 +24,10 @@ $(document).ready(function(){
 		}
 	});
 	
+	 $('.in-btn').click(function() {
+         	$(this).fadeIn('fast');
+    	 });
+    	 $('.out-btn').click(function() {
+         	$(this).fadeOut('fast');
+    	 });
 });
