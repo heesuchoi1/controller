1e296f09bc11ee5c323fb4c55be0a52ee473673d
최희수 <heesu7563@gmail.com>
2014-04-18 16:56:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 $(document).ready(function(){
-	   
-	   	$('#left-btn').click(function(){
-				$('#player').animate({left: "-=10px"}, 'fast');
-			});
 
-			$('#up-btn').click(function(){
-				$('#player').animate({top: "-=10px"}, 'fast');
-			});
-			
-			$('#right-btn').click(function(){
-				$('#player').animate({left: "+=10px"}, 'fast');
-			});
-			
-			$('#down-btn').click(function(){
-				$('#player').animate({top: "+=10px"}, 'fast');
-			});
-	   
-	    $('.controller').keydown(function(key) {
-        switch(parseInt(key.which,10)) {
-		case 37: //left
-		
-		case 38: //up
-		
-		case 39: //right
-			
-		case 40:
-		
-		}
+	$('#left-btn').click(function(){
+		$('#player').animate({left: "-=10px"}, 'fast');
 	});
+
+	$('#up-btn').click(function(){
+		$('#player').animate({left: "-=10px"}, 'fast')
+	});
+
+	$('#right-btn').click(function(){
+		$('#player').animate({left: "+=10px"}, 'fast');
+	});
+
+	$('#down-btn').click(function(){
+		$('#player').animate({top: "+=10px"}, 'fast');
+	});
+
+	$('.controller').keydown(function(key) {
+		switch(parseInt(key.which,10)) {
+		case 37: //left
+			$('#player').animate({left: "-=10px"}, 'fast')
+		case 38: //up
+			$('#player').animate({top: "-=10px"}, 'fast')
+		case 39: //right
+			$('#player').animate({left: "+=10px"}, 'fast')
+		case 40:
+			$('#player').animate({top:"+=10px"}, 'fast')	
+	}
+});
 	
-	 $('#in-btn').click(function() {
-         	$(this).fadeIn('fast');
-    	 });
-    	 $('#out-btn').click(function() {
-         	$(this).fadeOut('fast');
-    	 });
+	$('#in-btn').click(function() {
+		$(this).fadeIn('fast');
+	});
+	$('#out-btn').click(function() {
+		$(this).fadeOut('fast');
+	});
 });
